c5937d042046f97a4089e490ca681c101e95f20b
Cedric Liegeois <ofmooseandmen@yahoo.com>
2018-07-04 01:17:08 +1000

    More tests

--- test/Data/Geo/Jord/AngleSpec.hs
+++ test/Data/Geo/Jord/AngleSpec.hs
@@ -11,3 +11,13 @@ spec = do
     describe "Reading valid angles" $ do
         it "reads 55°36'21''" $ readAngle "55°36'21''" `angleShouldBe` ofDegrees 55.6058333
         it "reads 55.6058333°" $ readAngle "55.6058333°" `angleShouldBe` ofDegrees 55.6058333
+        it "reads -55.6058333°" $ readAngle "-55.6058333°" `angleShouldBe` ofDegrees (-55.6058333)
+    describe "Adding/Subtracting angles" $ do
+        it "adds angles" $ add (ofDegrees 55.6058333) (ofDegrees 5.0) `angleShouldBe` ofDegrees 60.6058333
+        it "subtracts angles" $ sub (ofDegrees 5.0) (ofDegrees 55.6058333) `angleShouldBe` ofDegrees (-50.6058333)
+    describe "degrees/radians conversion" $ do
+        it "converts degrees to radians" $ toRadians 55.6058333 `shouldBe` 0.9705048744001038
+        it "converts radians to degrees" $ toDegrees 2.6436343 `shouldBe` 151.46908796602173
+    describe "angle normalisation" $ do
+        it "it normalises radians in given range" $ normalise (ofRadians (3.0 * pi / 2.0)) 180.0 `angleShouldBe` ofDegrees 90.0
+        it "it normalises degrees in given range" $ normalise (ofDegrees 371.1) 360.0 `angleShouldBe` ofDegrees 11.1
--- test/Data/Geo/Jord/LengthSpec.hs
+++ test/Data/Geo/Jord/LengthSpec.hs
@@ -10,11 +10,21 @@ spec :: Spec
 spec = do
     describe "Reading valid lengths" $ do
         it "reads -15.2m" $ readLength "-15.2m" `lengthShouldBe` ofMetres (-15.2)
-    describe "Reading invalid lengthss" $ do
+        it "reads 154km" $ readLength "154km" `lengthShouldBe` ofKilometres 154
+        it "reads 1000Nm" $ readLength "1000Nm" `lengthShouldBe` ofNauticalMiles 1000
+    describe "Reading invalid lengths" $ do
         it "fails to read 5" $ readLengthM "5" `shouldBe` Nothing
+        it "fails to read 5nmi" $ readLengthM "5nmi" `shouldBe` Nothing
     describe "Showing lengths" $ do
         it "shows length in metres when <= 10000 m" $ show (ofMetres 5) `shouldBe` "5.0m"
         it "shows length in kilometres when > 10000 m" $
             show (ofKilometres 1000) `shouldBe` "1000.0km"
     describe "Converting lengths" $ do
         it "converts metres to kilometres" $ kilometres (ofMetres 1000) `shouldBe` 1.0
+        it "converts metres to nautical miles" $ nauticalMiles (ofMetres 1000) `shouldBe` 0.5399568034557235
+        it "converts kilometres to nautical miles" $ nauticalMiles (ofKilometres 1000) `shouldBe` 539.9568034557235
+        it "converts nautical miles to metres" $ metres (ofNauticalMiles 10.5) `shouldBe` 19446
+        it "converts nautical miles to kilometres" $ kilometres (ofNauticalMiles 10.5) `shouldBe` 19.446
+    describe "Adding/Subtracting lengths" $ do
+        it "adds lengths" $ add (ofKilometres 1000) (ofMetres 1000) `lengthShouldBe` ofMetres 1001000
+        it "subtracts lengths" $ sub (ofMetres 1000) (ofNauticalMiles 10.5) `lengthShouldBe` ofMetres (-18446)

95c09fd2dd7d0b00262b6f683e0de3376c12e314
Cedric Liegeois <cedric.liegeois@thalesgroup.com.au>
2018-07-03 21:23:54 +1000

    better REPL - init

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,7 @@ module Main where
 
 import Control.Monad (unless)
 import Data.Geo.Jord
+import Data.List
 import System.IO
 
 main :: IO ()
@@ -23,26 +24,46 @@ read' :: IO String
 read' = putStr "jord> " >> hFlush stdout >> getLine
 
 eval' :: String -> String
-eval' input =
-    case words input of
-        ["antipode", p] -> antipode' p
-        ["distance", p1, p2] -> distance' p1 p2
-        _ -> help
+eval' input = input
 
 print' :: String -> IO ()
 print' = putStrLn
 
-help :: String
-help = "Usage: antipode Position" ++ "\n       distance Postion Position" ++ "\n       :quit"
+data Result = Error String | OkGeo GeoPos | OkLen Length
 
-antipode' :: String -> String
-antipode' s = maybe ("Invalid position: " ++ s) (antipode >> show) (readGeoM s)
+data Variables = Variables
+    { positions :: [(String, GeoPos)]
+    }
 
-distance' :: String -> String -> String
-distance' s1 s2 =
-    case ps of
-        Nothing -> "Invalid position(s): " ++ s1 ++ ", " ++ s2
-        Just [p1, p2] -> show (distance p1 p2)
-        Just _ -> "Something went wrong..."
-  where
-    ps = mapM readGeoM [s1, s2]
+evaluate :: String -> Variables -> Result
+evaluate cmd vs =
+    case words cmd of
+        ["antipode", p] -> evalAntipode p vs
+        ["distance", p1, p2] -> evalDistance p1 p2 vs
+        _ -> Error cmd
+
+evalAntipode :: String -> Variables -> Result
+evalAntipode s vs =
+    case resolveAllPos [s] vs of
+        [OkGeo p] -> OkGeo (antipode p)
+        ps -> err ps
+
+evalDistance :: String -> String -> Variables -> Result
+evalDistance s1 s2 vs =
+    case resolveAllPos [s1, s2] vs of
+        [OkGeo p1, OkGeo p2] -> OkLen (distance p1 p2)
+        ps -> err ps
+
+resolveAllPos :: [String] -> Variables -> [Result]
+resolveAllPos xs vs = map (\l -> resolvePos l vs) xs
+
+resolvePos :: String -> Variables -> Result
+resolvePos s vs =
+    case filter (\a -> fst a == s) (positions vs) of
+        [(_, pos)] -> (OkGeo pos)
+        [] -> maybe (Error ("Invalid position: " ++ s)) OkGeo (readGeoPosM s) 
+        _ -> Error (s ++ " defined more than once")
+
+-- | Reduces all 'Error' results.
+err :: [Result] -> Result
+err rs = Error (intercalate "; " [e | Error e <- rs])

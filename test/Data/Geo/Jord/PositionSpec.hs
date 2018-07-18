1d3543f2169cd92263766ecebf41131fa9cfd67b
Cedric Liegeois <ofmooseandmen@yahoo.com>
2018-07-19 01:59:26 +1000

    assert ECEF vector with tolerance (0.00000001)

--- test/Data/Geo/Jord/PositionSpec.hs
+++ test/Data/Geo/Jord/PositionSpec.hs
@@ -2,7 +2,9 @@ module Data.Geo.Jord.PositionSpec
     ( spec
     ) where
 
+import Control.Monad
 import Data.Geo.Jord
+import Test.HUnit
 import Test.Hspec
 
 spec :: Spec
@@ -15,11 +17,11 @@ spec = do
                     , latLongHeight (latLongDecimal 48.8562 2.3508) 67.36972232195099
                     ]
             let refEcefs =
-                    [ ecefVector 5733855.774881717 (-6370998.380260889) 7008137.510624695
-                    , ecefVector 3194419.145121972 3194419.145121971 4487348.408606014
-                    , ecefVector 4200996.769740585 172460.32072401006 4780102.807809802
+                    [ ecefVector 5733855.77488171 (-6370998.38026088) 7008137.51062469
+                    , ecefVector 3194419.14512197 3194419.14512197 4487348.40860601
+                    , ecefVector 4200996.76974058 172460.32072401 4780102.80780980
                     ]
-            mapM_ (\(g, e) -> geodeticToEcef g wgs84 `shouldBe` e) (zip refGeodetics refEcefs)
+            mapM_ (\(g, e) -> geodeticToEcef g wgs84 `ecefShouldBe` e) (zip refGeodetics refEcefs)
     describe "ECEF To Geodetic" $
         it "transforms ECEF vector to geodetic position and height" $ do
             let refGeodetics =
@@ -33,3 +35,22 @@ spec = do
                     , ecefVector 4200996.769831858 172460.320727757 4780102.807914356
                     ]
             mapM_ (\(g, e) -> ecefToGeodetic e wgs84 `shouldBe` g) (zip refGeodetics refEcefs)
+
+ecefShouldBe :: (HasCallStack) => EcefVector -> EcefVector -> Expectation
+actual `ecefShouldBe` expected = do
+    assertDoubleEquals tolerance (ex expected) (ex actual)
+    assertDoubleEquals tolerance (ey expected) (ey actual)
+    assertDoubleEquals tolerance (ez expected) (ez actual)
+
+assertDoubleEquals ::
+       Double -- ^ The maximum difference between expected and actual
+    -> Double -- ^ The expected value
+    -> Double -- ^ The actual value
+    -> Expectation
+assertDoubleEquals delta expected actual =
+    unless (abs (expected - actual) < delta) (assertFailure msg)
+  where
+    msg = "expected: " ++ show expected ++ " +/- " ++ show delta ++ "\n but got: " ++ show actual
+
+tolerance :: Double
+tolerance = 0.00000001
